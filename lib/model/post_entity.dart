69e87af625fb9f25c46373be53e11b619a8bd633
axinger <liu_weixing@qq.com>
2020-08-13 14:00:15 +0800

    update…

--- lib/model/post_entity.dart
+++ lib/model/post_entity.dart
@@ -40,134 +40,14 @@ class PostEntity extends Object {
   }
 
   static List<PostEntity> dataList() {
-    List<Map<String, dynamic>> list = [
-      {
-        "title": "内容1",
-        "imageUrl":
-            "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2059216813,3169419971&fm=26&gp=0.jpg",
-        "author": "jim",
-      },
-      {
-        "title": "图片地址错误",
-        "imageUrl": "https://123.png",
-        "author": "jim",
-      },
-      {
-        "title": "内容2",
-        "imageUrl":
-            "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2641512116,3445406201&fm=26&gp=0.jpg",
-        "author": "tom",
-      },
-      {
-        "title": "内容3",
-        "imageUrl":
-            "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=186682105,1195550347&fm=26&gp=0.jpg",
-        "author": "tom",
-      },
-      {
-        "title": "内容4",
-        "imageUrl":
-            "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1671223440,1615374730&fm=26&gp=0.jpg",
-        "author": "tom",
-      },
-      {
-        "title": "内容5",
-        "imageUrl":
-            "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1731597746,428499852&fm=26&gp=0.jpg",
-        "author": "tom",
-      },
-      {
-        "title": "内容6",
-        "imageUrl":
-            "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=396448549,3340705754&fm=26&gp=0.jpg",
-        "author": "tom",
-      },
-      {
-        "title": "内容7",
-        "imageUrl":
-            "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1208538952,1443328523&fm=26&gp=0.jpg",
-        "author": "tom",
-      },
-      {
-        "title": "内容8",
-        "imageUrl":
-            "https://dss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1091579268,2185400060&fm=26&gp=0.jpg",
-        "author": "tom",
-      },
-      {
-        "title": "内容9",
-        "imageUrl":
-            "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=4214941395,707510718&fm=26&gp=0.jpg",
-        "author": "tom",
-      },
-    ];
-
-    return getPostEntityList(list);
-  }
-
-  static List<PostEntity> PostEntityDataList() {
-    List<Map<String, dynamic>> list = [
-      {
-        "title": "内容1",
-        "imageUrl":
-            "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2059216813,3169419971&fm=26&gp=0.jpg",
-        "author": "jim",
-      },
-      {
-        "title": "图片地址错误",
-        "imageUrl": "https://123.png",
-        "author": "jim",
-      },
-      {
-        "title": "内容2",
-        "imageUrl":
-            "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2641512116,3445406201&fm=26&gp=0.jpg",
-        "author": "tom",
-      },
-      {
-        "title": "内容3",
-        "imageUrl":
-            "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=186682105,1195550347&fm=26&gp=0.jpg",
-        "author": "tom",
-      },
-      {
-        "title": "内容4",
-        "imageUrl":
-            "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1671223440,1615374730&fm=26&gp=0.jpg",
-        "author": "tom",
-      },
-      {
-        "title": "内容5",
-        "imageUrl":
-            "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1731597746,428499852&fm=26&gp=0.jpg",
-        "author": "tom",
-      },
-      {
-        "title": "内容6",
-        "imageUrl":
-            "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=396448549,3340705754&fm=26&gp=0.jpg",
-        "author": "tom",
-      },
-      {
-        "title": "内容7",
-        "imageUrl":
-            "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1208538952,1443328523&fm=26&gp=0.jpg",
-        "author": "tom",
-      },
-      {
-        "title": "内容8",
-        "imageUrl":
-            "https://dss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1091579268,2185400060&fm=26&gp=0.jpg",
-        "author": "tom",
-      },
-      {
-        "title": "内容9",
-        "imageUrl":
-            "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=4214941395,707510718&fm=26&gp=0.jpg",
-        "author": "tom",
-      },
-    ];
-
+    List<Map<String, dynamic>> list = List.generate(20, (index) {
+      return {
+        'title': '内容-$index',
+        'imageUrl': 'https://bing.ioliu.cn/v1/rand?key=b$index&w=640&h=360',
+        'author': 'author-$index',
+      };
+    });
+    print('list == $list');
     return getPostEntityList(list);
   }
 }
