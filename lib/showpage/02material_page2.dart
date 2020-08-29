0f4f2e37d8fd919a924bda41ca9b2b89ac6fa7d5
axinger <liu_weixing@qq.com>
2020-08-29 10:27:03 +0800

    update…

--- lib/showpage/02material_page2.dart
+++ lib/showpage/02material_page2.dart
@@ -26,6 +26,7 @@ class _MaterialPage23 extends State<MaterialPage2> {
     super.initState();
     segmentedControlValue = segmentedControlData.keys.first;
   }
+
   @override
   void deactivate() {
     super.deactivate();
@@ -51,113 +52,187 @@ class _MaterialPage23 extends State<MaterialPage2> {
                   color: Colors.red,
                   child: Text('头'),
                 ),
+                Text('Container圆形,内容不会撑满'),
+                Container(
+                  height: 50,
+                  width: 50,
+
+                  /// 超出部分剪切
+                  clipBehavior: Clip.hardEdge,
+
+
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.purpleAccent,
+
+                    ///边框
+                    border: Border.all(color: Colors.grey, width: 2),
+
+                    /// 圆形
+                    shape: BoxShape.circle,
+
+                    /// 圆角
+//                    borderRadius: BorderRadius.all(Radius.circular(18)),
+                  ),
+                  child:  Column(
+                    children: [
+                      Text('Material'),
+                      Text('Material2'),
+                    ],
+                  ),
+                ),
+
+                Text('Material - StatefulWidget'),
+                Material(
+                  type: MaterialType.card,
+                  textStyle:TextStyle(color: Colors.orange, fontSize: 20),
+                  elevation: 1,
+                  color: Colors.red,
+
+                  /// 圆角
+//                   borderRadius:BorderRadius.circular(20),
+                  /// 边框 不能和圆角borderRadius 同时存在 ,圆角放shape 内部
+                  shape: CircleBorder(
+                    side: BorderSide(
+                      color: Colors.green,
+                      width: 2,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+
+//                    shape: RoundedRectangleBorder(
+//                      side: BorderSide(
+//                        width: 1,
+//                        color: Colors.green,
+//                      ),
+//
+//                      /// 圆角
+//                      borderRadius: BorderRadius.circular(20),
+//                    ),
+
+                  ///边界前景  ,超出部分是否显示
+                  borderOnForeground: false,
+
+                  /// 超出部分剪切
+                  clipBehavior: Clip.antiAlias,
+                  child: Column(
+                    children: [
+                      Text('Material'),
+                      Text('Material2'),
+                    ],
+                  ),
+                ),
+
                 Expanded(
 //              child: Container(
 //                height: 50,
 //                color: Colors.greenAccent,
 //              ),
 
-                  child: ListView(
-                    children: <Widget>[
-                      Text('TextFormField'),
-                      TextFormField(
-                        controller: passwordTextEditingController,
-                        focusNode: passwordFocusNode,
-                        obscureText: true,
+                  child: Container(
+                    color: Colors.indigoAccent,
+                    child: ListView(
+
+                      children: <Widget>[
+                        Text('TextFormField'),
+                        TextFormField(
+                          controller: passwordTextEditingController,
+                          focusNode: passwordFocusNode,
+                          obscureText: true,
 //                keyboardType: TextInputType.number,
 
-                        /// 输入的文字
-                        onChanged: (value) {
-                          print('onChanged输入的手机号' + value);
-                          setState(() {});
-                        },
+                          /// 输入的文字
+                          onChanged: (value) {
+                            print('onChanged输入的手机号' + value);
+                            setState(() {});
+                          },
 
-                        /// 相当于 onSubmitted 右下角
-                        onEditingComplete: () {
-                          print('onEditingComplete');
+                          /// 相当于 onSubmitted 右下角
+                          onEditingComplete: () {
+                            print('onEditingComplete');
 
-                          /// 收起键盘
-                          passwordFocusNode.unfocus();
-                        },
+                            /// 收起键盘
+                            passwordFocusNode.unfocus();
+                          },
 
-                        onSaved: (value) {
-                          debugPrint('保存密码 ' + value);
-                        },
+                          onSaved: (value) {
+                            debugPrint('保存密码 ' + value);
+                          },
 
-                        ///输入文本的样式
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.black,
-                        ),
-
-                        /// 限制输入类型
-                        inputFormatters: <TextInputFormatter>[
-                          LengthLimitingTextInputFormatter(20),
-                        ],
-
-                        validator: (value) {
-                          if (value.length < 6) {
-                            return '输入6位密码';
-                          }
-                          return null;
-                        },
-
-                        /// 自动验证
-                        autovalidate: true,
-
-                        decoration: InputDecoration(
-                          /// 头部的图标
-                          prefixIcon: Icon(
-                            Icons.lock,
-                          ),
-                          hintText: '请输入密码',
-                          hintStyle: TextStyle(
-                            fontSize: 16.0,
-                            color: Colors.red,
-                          ),
-
-                          labelText: '密码',
-                          labelStyle: TextStyle(
+                          ///输入文本的样式
+                          style: TextStyle(
                             fontSize: 16.0,
                             color: Colors.black,
                           ),
 
-                          ///输入内容距离上下左右的距离 ，可通过这个属性来控制 TextField的高度
-                          contentPadding: EdgeInsets.all(10.0),
-                          border: OutlineInputBorder(
+                          /// 限制输入类型
+                          inputFormatters: <TextInputFormatter>[
+                            LengthLimitingTextInputFormatter(20),
+                          ],
+
+                          validator: (value) {
+                            if (value.length < 6) {
+                              return '输入6位密码';
+                            }
+                            return null;
+                          },
+
+                          /// 自动验证
+                          autovalidate: true,
+
+                          decoration: InputDecoration(
+                            /// 头部的图标
+                            prefixIcon: Icon(
+                              Icons.lock,
+                            ),
+                            hintText: '请输入密码',
+                            hintStyle: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.red,
+                            ),
+
+                            labelText: '密码',
+                            labelStyle: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.black,
+                            ),
+
+                            ///输入内容距离上下左右的距离 ，可通过这个属性来控制 TextField的高度
+                            contentPadding: EdgeInsets.all(10.0),
+                            border: OutlineInputBorder(
 //                    borderRadius: BorderRadius.circular(15),
 //                    borderSide: BorderSide.none,
-                              ),
+                                ),
+                          ),
                         ),
-                      ),
-                      Text('TextField'),
-                      TextField(
+                        Text('TextField'),
+                        TextField(
 //                    controller: phoneTextEditingController,
 //                    focusNode: _contentFocusNode,
 
-                        /// 输入的文字
-                        onChanged: (value) {
-                          print('TextField = ' + value);
-                        },
-                        onEditingComplete: () {},
+                          /// 输入的文字
+                          onChanged: (value) {
+                            print('TextField = ' + value);
+                          },
+                          onEditingComplete: () {},
 
 //                    maxLength: 11,
 
-                        ///键盘类型
+                          ///键盘类型
 //                    keyboardType: TextInputType.number,
 
-                        /// 右下角 键盘类型
-                        textInputAction: TextInputAction.send,
-                        onSubmitted: (value) {
-                          print('提交手机号' + value);
-                        },
+                          /// 右下角 键盘类型
+                          textInputAction: TextInputAction.send,
+                          onSubmitted: (value) {
+                            print('提交手机号' + value);
+                          },
 
-                        inputFormatters: <TextInputFormatter>[
+                          inputFormatters: <TextInputFormatter>[
 //                      WhitelistingTextInputFormatter.digitsOnly, //只输入数字
 //                      TestTextInputFormatter(),
-                          FilteringTextInputFormatter(RegExp("[a-zA-Z]"),
-                              allow: true),
-                          //只允许输入字母
+                            FilteringTextInputFormatter(RegExp("[a-zA-Z]"),
+                                allow: true),
+                            //只允许输入字母
 
 //                      WhitelistingTextInputFormatter(RegExp(r'(\d+)(.\d{0,2})?$/')),
 
@@ -166,7 +241,7 @@ class _MaterialPage23 extends State<MaterialPage2> {
 //                      ///零和非零开头的数字
 //                      WhitelistingTextInputFormatter(RegExp(r'^(0|[1-9][0-9]*)$'))
 
-                          ///有两位小数的正实数
+                            ///有两位小数的正实数
 //                      WhitelistingTextInputFormatter(RegExp(r'^[a-zA-Z][a-zA-Z0-9_]{4,15}$'))
 
 //                      WhitelistingTextInputFormatterExtension.chinesePhone,
@@ -179,143 +254,102 @@ class _MaterialPage23 extends State<MaterialPage2> {
 
 //                      WhitelistingTextInputFormatter,
 //                  PhoneTextInputFormatter.digitsOnly,
-                        ],
+                          ],
 
-                        ///输入文本的样式
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.black,
-                        ),
-
-                        decoration: InputDecoration(
-                          /// 头部的图标
-                          icon: Icon(
-                            Icons.phone,
-                          ),
-//                      prefix: Text('prefix'),
-//                      prefixText: 'prefixText',
-                          prefixIcon: Text('prefixIcon'),
-//
-//                      suffix: Text('suffix'),
-                          suffixIcon: Text('suffixIcon'),
-
-                          hintText: '请输入手机号',
-
-                          /// 左上角的标识
-                          labelText: '手机号',
-                          hintStyle: TextStyle(
-//                            fontSize: 16.0,
+                          ///输入文本的样式
+                          style: TextStyle(
+                            fontSize: 16.0,
                             color: Colors.black,
                           ),
 
-                          ///输入内容距离上下左右的距离 ，可通过这个属性来控制 TextField的高度
-                          contentPadding: EdgeInsets.all(10.0),
+                          decoration: InputDecoration(
+                            /// 头部的图标
+                            icon: Icon(
+                              Icons.phone,
+                            ),
+//                      prefix: Text('prefix'),
+//                      prefixText: 'prefixText',
+                            prefixIcon: Text('prefixIcon'),
+//
+//                      suffix: Text('suffix'),
+                            suffixIcon: Text('suffixIcon'),
 
-                          /// 这个无效
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(100),
-                            borderSide: BorderSide(width: 10),
-                          ),
+                            hintText: '请输入手机号',
 
-                          ///选中时外边框颜色
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
+                            /// 左上角的标识
+                            labelText: '手机号',
+                            hintStyle: TextStyle(
+//                            fontSize: 16.0,
+                              color: Colors.black,
+                            ),
+
+                            ///输入内容距离上下左右的距离 ，可通过这个属性来控制 TextField的高度
+                            contentPadding: EdgeInsets.all(10.0),
+
+                            /// 这个无效
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100),
+                              borderSide: BorderSide(width: 10),
+                            ),
+
+                            ///选中时外边框颜色
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
 //                        borderSide: BorderSide(
 //                          color: Colors.red,
 //                        ),
 
-                            /// 无边框
-                            borderSide: BorderSide.none,
-                          ),
+                              /// 无边框
+                              borderSide: BorderSide.none,
+                            ),
 
-                          ///未选中时候的颜色
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(
-                              color: Colors.grey, //边线颜色为白色
+                            ///未选中时候的颜色
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                color: Colors.grey, //边线颜色为白色
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(bottom: 50),
-                        child: Material(
-                          type: MaterialType.card,
-                          textStyle:
-                              TextStyle(color: Colors.orange, fontSize: 20),
-                          elevation: 1,
-                          color: Colors.red,
 
-                          /// 圆角
-//                   borderRadius:BorderRadius.circular(20),
-                          /// 边框 不能和圆角borderRadius 同时存在 ,圆角放shape 内部
-                          shape: CircleBorder(
-                            side: BorderSide(
-                              color: Colors.green,
-                              width: 2,
-                              style: BorderStyle.solid,
-                            ),
-                          ),
-
-//                    shape: RoundedRectangleBorder(
-//                      side: BorderSide(
-//                        width: 1,
-//                        color: Colors.green,
-//                      ),
-//
-//                      /// 圆角
-//                      borderRadius: BorderRadius.circular(20),
-//                    ),
-
-                          ///边界前景  ,超出部分是否显示
-                          borderOnForeground: false,
-
-                          /// 超出部分剪切
-                          clipBehavior: Clip.antiAlias,
-                          child: Column(
-                            children: [
-                              Text('Material'),
-                              Text('Material2'),
-                            ],
-                          ),
-                        ),
-                      ),
-                      FlatButton(
-                        child: Text('aa'),
-                        onPressed: () {
-                          Net.post('1').success((value) {
-                            print('success = $value');
-                          }).failure((value) {
-                            print('failure = $value');
-                          });
+                        FlatButton(
+                          child: Text('aa'),
+                          onPressed: () {
+                            Net.post('1').success((value) {
+                              print('success = $value');
+                            }).failure((value) {
+                              print('failure = $value');
+                            });
 
 //                    Future.delayed(Duration(seconds: 0),(){
 //                      return 'jim';
 //                    }).then((value){
 //                      print('failure = $value');
 //                    });
-                        },
-                      ),
-                      CupertinoSegmentedControl<String>(
-                        children: segmentedControlData,
-                        // 数据
-                        groupValue: segmentedControlValue,
-                        // 选中的数据
-                        onValueChanged: (fruit) {
-                          setState(() {
-                            // 数据改变时通过setState改变选中状态
-                            segmentedControlValue = fruit;
-                          });
-                        },
-                        unselectedColor: CupertinoColors.white,
-                        // 未选中颜色
-                        selectedColor: CupertinoColors.activeBlue,
-                        // 选中颜色
-                        borderColor: CupertinoColors.activeBlue,
-                        // 边框颜色
-                        pressedColor: const Color(0x33007AFF), // 点击时候的颜色
-                      ),
-                    ],
+                          },
+                        ),
+                        CupertinoSegmentedControl<String>(
+                          children: segmentedControlData,
+                          // 数据
+                          groupValue: segmentedControlValue,
+                          // 选中的数据
+                          onValueChanged: (fruit) {
+                            setState(() {
+                              // 数据改变时通过setState改变选中状态
+                              segmentedControlValue = fruit;
+                            });
+                          },
+                          unselectedColor: CupertinoColors.white,
+                          // 未选中颜色
+                          selectedColor: CupertinoColors.activeBlue,
+                          // 选中颜色
+                          borderColor: CupertinoColors.activeBlue,
+                          // 边框颜色
+                          pressedColor: const Color(0x33007AFF), // 点击时候的颜色
+                        ),
+                      ],
+                    ),
                   ),
                 ),
                 Container(
